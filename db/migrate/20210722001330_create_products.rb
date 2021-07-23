17b67dbf72e502d90ae966b5ae009c3ca58bceba
AcePrincipal <acepetrov96@GMAIL.com>
2021-07-23 03:27:12 -0500

    Seeded my database with 3 products

--- db/migrate/20210722001330_create_products.rb
+++ db/migrate/20210722001330_create_products.rb
@@ -2,6 +2,7 @@ class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
       t.string :name
+      t.string :pic 
       t.string :description
       t.string :option1
       t.string :option2
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: 'Proprietary Software', pic: 'https://blog.planview.com/wp-content/uploads/2020/01/Top-6-Software-Development-Methodologies.jpg', description: 'Our team will create and deliver proprietary software for your company to use in-house.', option1: 'Administrative', option2: 'Functional', option3: 'Customer Interactive');
+Product.create(name: 'Database Installation', pic: 'https://s7280.pcdn.co/wp-content/uploads/2016/06/database-blue.png', description: 'Our team will create and set up a database system for your company.', option1: 'Multiple Sites', option2: 'Maintanence', option3: 'Customer Interactive');
+Product.create(name: 'Local Networks', pic: 'https://3.bp.blogspot.com/-SFfhwbJjcnM/WKqGSsv55dI/AAAAAAAAAAs/V9F_nnjkuFs9qhgBC93aDMjQOanlSSITwCLcB/w1200-h630-p-k-no-nu/connect.jpg', description: 'Our team will set up a local network for your company to use in-house', option1: 'Fiber', option2: 'Multiple Sites', option3: 'Added Security');
